152542954664dead688087f7a7544a8ed43e105b
Josh Palmer <Josh.Palmer@mendix.com>
2020-07-03 16:14:30 +0200

    Add HEAD reference for Openfortivpn.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -1,5 +1,6 @@
 class Openfortivpn < Formula
   desc "Open Fortinet client for PPP+SSL VPN tunnel services"
+  head "https://github.com/adrienverge/openfortivpn.git"
   homepage "https://github.com/adrienverge/openfortivpn"
   url "https://github.com/adrienverge/openfortivpn/archive/v1.14.1.tar.gz"
   sha256 "bc62fc6ecaaa6c6f8f2510e14a067a0cb9762158d9460c04555990bba44b50ca"
